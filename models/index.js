cb6cb6c1f3ca515c1cd6ea4fff6a93929282c52c
abcbinch <colorquilt112358@gmail.com>
2024-12-16 18:17:57 +0900

    2차

--- models/index.js
+++ models/index.js
@@ -9,29 +9,6 @@ const db = {};
 
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-// fs.readdirSync(__dirname)
-//   .filter((file) => {
-//     return (
-//       file.indexOf(".") !== 0 &&
-//       file !== basename &&
-//       file.slice(-3) === ".js" &&
-//       file.indexOf(".test.js") === -1
-//     );
-//   })
-//   .forEach((file) => {
-//     const model = require(path.join(__dirname, file))(
-//       sequelize,
-//       Sequelize.DataTypes
-//     );
-//     db[model.name] = model;
-//   });
-
-// Object.keys(db).forEach((modelName) => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
