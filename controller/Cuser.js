f976f787622aa7e087e0b1d4f12929a6263e4713
yundda <vvvzzang1218@naver.com>
2024-12-18 14:18:10 +0900

    [Feat/Remove] mealtime 파일 삭제 및 controller 수정

--- controller/Cuser.js
+++ controller/Cuser.js
@@ -1,14 +1,23 @@
-// const models = require("../models/User");
+const utils = require("../utils/utils");
+const models = require("../models");
+const {
+  calc_AMR,
+  calc_BMR,
+  calc_intake,
+  calc_carbo,
+  calc_protein,
+  calc_fat,
+} = require("../../test_buddy1/utils/utils");
 
 exports.getUser = (req, res) => {
-  res.render("index");
+  res.render("user");
 };
 
 exports.getSetGoal = (req, res) => {
   res.render("settingGoal");
 };
 
-exports.getDailyInput = (req, res) => {
+exports.getDailyIntake = (req, res) => {
   res.render("user");
 };
 
@@ -20,6 +29,131 @@ exports.patchUser = (req, res) => {
   res.send("회원 정보 수정 완료");
 };
 
-exports.deleteUser = (req, res) => {
-  res.send("회원 탈퇴 완료");
+exports.deleteUser = async (req, res) => {
+  try {
+    const Result = await models;
+    res.send("회원 탈퇴 완료");
+  } catch (err) {
+    console.log("Cuser.js postIntake : server error", err);
+    res.status(500).send("Cuser.js postIntake : server error");
+  }
 };
+
+/* req.session.user = {id, email, name} */
+
+// [241218] 추가
+// POST '/user/settingGoal
+// 목표 설정 DB 저장 (create)
+exports.postSetGoal = async (req, res) => {
+  const { id: sessionId } = req.session.user;
+  // gender value -> "male", "female"
+  // activeLevel value -> 1.2 / 1.375 / 1.55 / 1.725
+  // dietGoal value -> "근성장" / "체지방 감량" / "유지"
+  const { weight, height, age, gender, activeLevel, goalWeight, period, dietGoal } =
+    req.body;
+  const calcedBMR = calc_BMR(gender, weight, height, age);
+  const calcedAMR = calc_AMR(calcedBMR, activeLevel);
+  const calcedIntake = calc_intake(calcedAMR, weight, goalWeight, period);
+  const calcedCarbo = calc_carbo(calcedIntake, dietGoal);
+  const calcedProtein = calc_protein(gender, activeLevel, dietGoal, weight);
+  const calcedFat = calc_fat(calcedIntake, calcedCarbo, calcedProtein);
+  try {
+    const checkGoal = await models.UserGoal.findOne({
+      where: {
+        id: sessionId,
+      },
+    });
+    if (checkGoal) {
+      const goalResult = await models.UserGoal.update(
+        {
+          weight: weight,
+          height: height,
+          age: age,
+          gender: gender,
+          activeLevel: activeLevel,
+          goalWeight: goalWeight,
+          period: period,
+          dietGoal: dietGoal,
+          BMR: calcedBMR,
+          AMR: calcedAMR,
+          recomIntake: calcedIntake,
+          recomCarbo: calcedCarbo,
+          recomProtein: calcedProtein,
+          recomFat: calcedFat,
+        },
+        {
+          where: {
+            id: sessionId,
+          },
+        }
+      );
+    } else {
+      const goalResult = await models.UserGoal.create(
+        {
+          weight: weight,
+          height: height,
+          age: age,
+          gender: gender,
+          activeLevel: activeLevel,
+          goalWeight: goalWeight,
+          period: period,
+          dietGoal: dietGoal,
+          BMR: calcedBMR,
+          AMR: calcedAMR,
+          recomIntake: calcedIntake,
+          recomCarbo: calcedCarbo,
+          recomProtein: calcedProtein,
+          recomFat: calcedFat,
+        },
+        {
+          where: {
+            id: sessionId,
+          },
+        }
+      );
+    }
+    res.send("유저 목표 DB 저장 성공");
+  } catch (err) {
+    console.log("Cuser.js postSetGoal : server error", err);
+    res.status(500).send("Cuser.js postSetGoal : server error");
+  }
+};
+
+// POST '/user/dailyIntake'
+// 섭취량 DB 저장 (create)
+exports.postIntake = async (req, res) => {
+  // req.body or form 데이터
+  // mealtime value -> "breakfast", "lunch", "dinner", "btwmeal"
+  const { mealtime, carbo, protein, fat, fiber } = req.body;
+  try {
+    const intakeResult = await models.Intake.create(
+      {
+        mealtime: mealtime,
+        carbo: carbo,
+        protein: protein,
+        fat: fat,
+        fiber: fiber,
+      },
+      {
+        where: {
+          id: sessionId,
+        },
+      }
+    );
+    res.send("유저 섭취량 DB 저장 성공");
+  } catch (err) {
+    console.log("Cuser.js postIntake : server error", err);
+    res.status(500).send("Cuser.js postIntake : server error");
+  }
+};
+
+// // POST '/user/dailyIntake'
+// // 섭취량 DB 저장 (create)
+// exports.postIntake =  async (req,res)=>{
+//   try{
+//     const Result = await models
+//   } catch (err) {
+//     console.log("Cuser.js postIntake : server error", err);
+//     res.status(500).send("Cuser.js postIntake : server error");
+//   }
+// }
