efeb40cb3e13401df1da6905ab864a24638a5b5b
aminaakh1680 <aminaakh1680@gmail.com>
2024-12-23 17:40:20 +0900

    [Fix/userupdatepage]

--- static/js/userUpdate.js
+++ static/js/userUpdate.js
@@ -3,7 +3,9 @@ console.log("update axios 정상작동");
 function user_update() {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
-  const pw = document.getElementById("password").value;
+  const pw1 = document.getElementById("password1").value;
+  const pw2 = document.getElementById("password2").value;
+
   const findPw = document.getElementById("security-answer").value;
   const regPw = /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]{8,20}$/;
 
@@ -11,12 +13,16 @@ function user_update() {
     alert("이름을 입력해 주세요.");
     return false;
   }
-  if (findPw === "") {
-    alert("비밀번호 확인 답변을 입력해 주세요.");
+  // if (findPw === "") {
+  //   alert("비밀번호 확인 답변을 입력해 주세요.");
+  //   return false;
+  // }
+  if (pw1 !== "" && !regPw.test(pw1)) {
+    alert("비밀번호는 영어 대소문자와 숫자를 포함한 8~20자리 글자여야 합니다.");
     return false;
   }
-  if (pw !== "" && !regPw.test(pw)) {
-    alert("비밀번호는 영어 대소문자와 숫자를 포함한 8~20자리 글자여야 합니다.");
+  if (!pw1 === pw2) {
+    alert("비밀번호가 일치하지 않습니다.다시 확인해주세요.");
     return false;
   }
 
