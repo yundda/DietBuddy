cdd8911d3dd401966ebf86844ab8cf74c5358820
soyung sim <simsy1024@gmail.com>
2024-12-23 20:47:45 +0900

    [Feature] find pw, signup 페이지 수정

--- static/js/findPw_axios.js
+++ static/js/findPw_axios.js
@@ -1,4 +1,8 @@
 function user_new_pw() {
+  // 이메일과 답변이 서버에 저장된 값과 같다면
+  // 새로운 비번 바꿀 수 있는 새 창을 보여주고 -> 확인 누르면 로그인화면으로
+  // 만약 답변과 이메일이 맞지 않다면 얼럿 띄워주기
+  
   const form = document.forms["new-password"];
 
   axios({
