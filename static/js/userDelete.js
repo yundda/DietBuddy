593d2ac602219f4e11f717752324fec7be26a733
aminaakh1680 <aminaakh1680@gmail.com>
2024-12-25 15:07:11 +0900

    [Feature/settingGoalpage modal]

--- static/js/userDelete.js
+++ static/js/userDelete.js
@@ -1,5 +1,7 @@
 function delete_user() {
-  const ask = confirm("탈퇴하시겠습니까?");
+  const ask = confirm(
+    "탈퇴하시겠습니까?\n회원 탈퇴시 이전 회원 정보 복구가 어려운 점 주위하시기 바랍니다"
+  );
 
   if (ask) {
     axios({
--- static/js/userUpdate.js
+++ static/js/userUpdate.js
@@ -1,4 +1,3 @@
-console.log("update axios 정상작동");
 const originalName = document.getElementById("name").value;
 function isClick() {
   const pw1 = document.getElementById("password1");
@@ -52,5 +51,23 @@ function user_update() {
     })
     .catch((err) => {
       console.log(err);
+      if (err.response) {
+        const { status, data } = err.response;
+        if (status === 400) {
+          alert("요청 데이터가 잘못되었습니다. 다시 확인해주세요.");
+        } else if (status === 404) {
+          alert("요청한 리소스를 찾을 수 없습니다. URL을 확인해주세요.");
+        } else if (status === 500) {
+          alert("서버에서 문제가 발생했습니다. 잠시 후 다시 시도해주세요.");
+        } else {
+          alert(`알 수 없는 오류가 발생했습니다. (상태 코드: ${status})`);
+        }
+      } else if (err.request) {
+        alert(
+          "서버에서 응답이 없습니다. 인터넷 연결을 확인하거나 잠시 후 다시 시도해주세요."
+        );
+      } else {
+        alert("요청을 처리하는 중 오류가 발생했습니다. 콘솔을 확인해주세요.");
+      }
     });
 }
