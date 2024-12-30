7b5027f59523628a82180f7d29dca475ef85cadd
soyung sim <simsy1024@gmail.com>
2024-12-30 09:36:59 +0900

    [Feature] 날짜별 식단 조회

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get("/signup", controller.getSignup);
 router.get("/login", controller.getLogin);
 router.get("/findPw", controller.getFindpw); //비번찾기 페이지
 router.get("/mypage/:date?", controller_mypage.getMypage);
-router.get("/todayIntake", controller_mypage.getTodayIntake);
+router.get("/todayIntake/:date?", controller_mypage.getTodayIntake);
 
 router.post("/doSignup", controller.postSignup);
 router.post("/doLogin", controller.postLogin);
